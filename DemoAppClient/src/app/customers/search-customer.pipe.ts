ba883555f26e1df6172b9f0a54c70fe7cb5e78dc
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-07-13 15:11:57 +0400

    search box

--- DemoAppClient/src/app/customers/search-customer.pipe.ts
+++ DemoAppClient/src/app/customers/search-customer.pipe.ts
@@ -5,15 +5,23 @@ import { CustomersComponent } from './customers.component';
   name: 'searchCustomer'
 })
 export class SearchCustomerPipe implements PipeTransform {
-
   transform(
-    value: { firstName: string; lastName: string; age: number } [], 
-    searchValue: string,
-    ):  { firstName: string; lastName: string; age: number} [] {
+    value: {
+      firstName: string;
+      lastName: string;
+      age: number;
+    }[],
+    searchValue: string
+  ): {
+    firstName: string;
+    lastName: string;
+    age: number;
+  }[] {
     if (searchValue === '') {
       return value;
-    } 
-    return value.filter((customers) => customers.firstName.includes(searchValue))
+    }
+    return value.filter((customer) =>
+      customer.firstName.toLowerCase().includes(searchValue)
+    );
   }
-
 }
